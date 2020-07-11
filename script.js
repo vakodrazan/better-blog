3f956a32e645388d7918696c1caded0af07e87d1
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-07-11 10:42:17 +0300

    Create the html and add it at the beginning of the container

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('it works');
+const postList = document.querySelector('#post-list');
+const postTitle = document.querySelector('.form-control#postTitle');
+const postContent = document.querySelector('.form-control[name="postContent"]');
+const author = document.querySelector('.form-control[name="postAuthor"]');
+const PostImage = document.querySelector('.form-control[name="postImg"]');
+const submitButton = document.querySelector(".btn.btn-primary");
+const form = document.querySelector('#post-form')
+
+submitButton.addEventListener('click', ($event) => {
+    $event.preventDefault();
+    const blogPost = `
+        <div class="card">
+            <img
+                class="card-img-top"
+                src="${PostImage.value}"
+                alt="Card image cap"
+                width = "500"
+                height = "200"
+            />
+            <div class="card-body">
+                <h5 class="card-title">
+                ${postTitle.value}
+                <small>by ${author.value}</small>
+                </h5>
+                <p class="card-text">${postContent.value}</p>
+                <button type="button" class="btn btn-sm btn-light btn-delete">
+                    Delete entry
+                </button>
+            </div>
+        </div>
+    `;
+    postList.insertAdjacentHTML('afterbegin', blogPost);
+    form.reset();
+});
